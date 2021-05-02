526da256e6e8a7f60187c83438ccb6c2eed3a7f7
Sndergaard <emilsndergaard>
2021-05-02 15:18:59 +0200

    Add project files.

---
+++ SentimentAnalysisFunctionsApp/DataModels/SentimentData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace SentimentAnalysisFunctionsApp.DataModels
+{
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText;
+
+        [LoadColumn(1)]
+        [ColumnName("Label")]
+        public bool Sentiment;
+    }
+}
---
+++ SentimentAnalysisFunctionsApp/DataModels/SentimentPrediction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace SentimentAnalysisFunctionsApp.DataModels
+{
+    public class SentimentPrediction : SentimentData
+    {
+
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
